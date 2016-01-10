2ae6b02df6126f4ca2698e09962a9401dafea35e
Peter Bengtsson <mail@peterbe.com>
2016-01-10 13:42:01 -0500

    gulp file

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+// grab our gulp packages
+var gulp  = require('gulp');
+var htmlreplace = require('gulp-html-replace');
+var fs = require('fs');
+
+
+gulp.task('buildHtml', function() {
+  gulp.src('index.html')
+  .pipe(htmlreplace({
+        'trackjs': fs.readFileSync('trackjs.html', 'utf8'),
+    }))
+  .pipe(gulp.dest('dist/'));
+});
+
+
+gulp.task('default', ['buildHtml']);
