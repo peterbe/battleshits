19fd3ad1363da99120242e2ab56581b91c6cfca0
Peter Bengtsson <mail@peterbe.com>
2016-01-11 08:53:41 -0500

    __dev__ 2

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,7 +1,13 @@
+var webpack = require('webpack');
+
 var path = require('path');
 var node_modules_dir = path.resolve(__dirname, 'node_modules');
 
-var config = {
+var definePlugin = new webpack.DefinePlugin({
+  __DEV__: false,
+});
+
+module.exports = {
   entry: path.resolve(__dirname, 'src/js/app.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -16,7 +22,8 @@ var config = {
       exclude: [node_modules_dir],
       loader: 'babel'
     }]
-  }
+  },
+  plugins: [
+    definePlugin,
+  ]
 };
-
-module.exports = config;
