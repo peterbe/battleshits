9e7e399ab2a6f02ca20e27c14b4d60c9c9aa6c02
Peter Bengtsson <mail@peterbe.com>
2016-03-02 11:20:28 -0500

    minify inline

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 // grab our gulp packages
 var gulp  = require('gulp');
 var htmlreplace = require('gulp-html-replace');
+var minifyInline = require('gulp-minify-inline');
 var fs = require('fs');
 
 
 gulp.task('buildHtml', function() {
   gulp.src('index.html')
+  .pipe(minifyInline({
+    jsSelector: 'pleasedontminifyscripts'
+  }))
   .pipe(htmlreplace({
         'trackjs': fs.readFileSync('src/snippets/trackjs.html', 'utf8'),
         'rollbar': fs.readFileSync('src/snippets/rollbar.html', 'utf8'),
