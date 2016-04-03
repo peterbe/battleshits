56eee95dd14a930e06341f41e9afa4e7970fc577
Peter Bengtsson <mail@peterbe.com>
2016-04-03 15:59:12 -0400

    performance optimization of bomb

--- src/js/components/bomb.jsx
+++ src/js/components/bomb.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import $ from 'jquery';
+import React from 'react'
+import $ from 'jquery'
+import shallowCompare from 'react-addons-shallow-compare'
 
 
 export default class Bomb extends React.Component {
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return shallowCompare(this, nextProps, nextState)
+  }
 
   render() {
     let src = {
