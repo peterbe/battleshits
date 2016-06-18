a5f2b1681ad88110dd10f60ac206b5390f768453
Peter Bengtsson <mail@peterbe.com>
2016-06-18 20:47:09 +0100

    pure functional component (bomb)

--- src/js/components/bomb.jsx
+++ src/js/components/bomb.jsx
@@ -1,51 +1,54 @@
 import React from 'react'
-import shallowCompare from 'react-addons-shallow-compare'
+// import shallowCompare from 'react-addons-shallow-compare'
 
+let width = 1
+let height = 1
 
-// XXX rewrite this as a presentationl functional component since there is no state
-export default class Bomb extends React.Component {
+window.addEventListener("resize", () => {
+  width = 1
+  height = 1
+})
 
-  shouldComponentUpdate(nextProps, nextState) {
-    return shallowCompare(this, nextProps, nextState)
-  }
-
-  getWidthHeight() {
+const getWidthHeight = () => {
+  if (width === 1 && height === 1) {
     let trElement = document.querySelector('.grid tr')
-    let width = 1
     if (trElement !== null) {
       width = trElement.clientWidth / 10
     }
     let tableElement = document.querySelector('.grid table')
-    let height = 1
     if (tableElement !== null) {
       height = tableElement.clientHeight / 10
     }
-    return {width: width, height: height}
   }
-
-  render() {
-    let src = {
-      1: "static/images/poop.png",
-      2: "static/images/explosion.png",
-      3: "static/images/toiletroll.png",
-    }[this.props.state]
-
-    let index = this.props.index
-    let x = index % 10
-    let y = Math.floor(index / 10)
-
-    let {width, height} = this.getWidthHeight()
-
-    let borderWidth = 0 // for each <td> (collapsed table)
-    let style = {
-      top: y * height + y * borderWidth,
-      left: x * width + x * borderWidth,
-      width: width, height: height,
-    }
-    return <img
-      className="bomb"
-      src={src}
-      style={style}
-      />
-  }
+  return { width, height }
 }
+
+
+// map "state" to image URL
+const imageURLs = {
+  1: "static/images/poop.png",
+  2: "static/images/explosion.png",
+  3: "static/images/toiletroll.png",
+}
+
+const Bomb = ({ state, index }) => {
+
+  let src = imageURLs[state]
+  let x = index % 10
+  let y = Math.floor(index / 10)
+
+  let { width, height } = getWidthHeight()
+
+  let borderWidth = 0 // for each <td> (collapsed table)
+  let style = {
+    top: y * height + y * borderWidth,
+    left: x * width + x * borderWidth,
+    width: width, height: height,
+  }
+  return <img
+    className="bomb"
+    src={src}
+    style={style}
+    />
+}
+export default Bomb
